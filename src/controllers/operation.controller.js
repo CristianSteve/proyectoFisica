00c809cfafc785b15533157d33cff04b4b61d017
Cristian Steve <ccristiansteve14@gmail.com>
2021-04-24 13:32:56 -0500

    Operaciones tiempo y velocidad final

--- src/controllers/operation.controller.js
+++ src/controllers/operation.controller.js
@@ -1,5 +1,55 @@
 operationsProyect = {};
 
-operationsProyect.findVelocuty = () => {};
+operationsProyect.findMethod = async (req, res) => {
+	const { operationType } = req.body;
+
+	if (!operationType) {
+		res.status(409).json({
+			//	message: "Parametros obligatorios no informados",
+			message: "Tipo de operacion no informado",
+		});
+	} else {
+		let response = 0;
+		if (operationType == "tiempo") response = await findWeather(req);
+		if (operationType == "vfinal") response = await findVFinal(req);
+		res.json({ message: { operacion: response } });
+	}
+};
+
+findVFinal = async (req) => {
+	const { initialVelocity, acceleration, weather } = req.body;
+	const distancia = await calculateDistance(
+		initialVelocity,
+		weather,
+		acceleration
+	);
+
+	const vfinal = initialVelocity + acceleration * weather;
+
+	return { vfinal: vfinal, distancia: distancia };
+};
+
+findWeather = async (req) => {
+	const { initialVelocity, finalSpeed, acceleration } = req.body;
+	const tiempo = await calculateTime(
+		initialVelocity,
+		finalSpeed,
+		acceleration
+	);
+
+	const distancia = await calculateDistance(
+		initialVelocity,
+		tiempo,
+		acceleration
+	);
+
+	return { tiempo: tiempo, distancia: distancia };
+};
+
+calculateTime = async (initialVelocity, finalSpeed, acceleration) =>
+	(finalSpeed - initialVelocity) / acceleration;
+
+calculateDistance = async (initialVelocity, tiempo, acceleration) =>
+	initialVelocity * tiempo + (1 / 2) * acceleration * (tiempo * tiempo);
 
 module.exports = operationsProyect;
--- src/routers/operations.js
+++ src/routers/operations.js
@@ -2,8 +2,9 @@ const { Router } = require("express");
 
 const router = Router();
 
-const { findVelocuty } = require("../controllers/operation.controller");
+const { findMethod } = require("../controllers/operation.controller");
 
-router.route("/mrua").get(findVelocuty);
+router.route("/mrua").get(findMethod);
+router.route("/mrua").post(findMethod);
 
 module.exports = router;
